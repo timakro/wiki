7e6de65f603e1839c873301e9e491d7517914b45
Tim Schumacher <tim@tschumacher.net>
2026-05-19 22:25:46 +0200

    Add vite-plugin-pwa

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,13 @@ import { defineConfig } from 'vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     tailwindcss(),
+    VitePWA({ registerType: 'autoUpdate' }),
     react(),
     babel({ presets: [reactCompilerPreset()] })
   ],
